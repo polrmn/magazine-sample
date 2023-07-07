52854921600957922d0d1220c4d03624a1bddd79
polrmn <polrmn@gmail.com>
2023-07-07 17:57:13 +0300

    change slider

--- src/App.js
+++ src/App.js
@@ -1,73 +1,99 @@
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
-import "swiper/css/zoom";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-import "swiper/css/effect-cube";
+// import { Swiper, SwiperSlide } from "swiper/react";
+// import "swiper/css";
+// import "swiper/css/zoom";
+// import "swiper/css/navigation";
+// import "swiper/css/pagination";
+// import "swiper/css/effect-cube";
 // import "swiper/css/effect-flip";
 // import "swiper/css/lazy";
 
-import { Zoom, Navigation, EffectCube, Pagination } from "swiper/modules";
+// import { Zoom, Navigation, EffectCube, Pagination } from "swiper/modules";
 
-import titleImage from "./assets/Cola_1/title.jpg";
+// import titleImage from "./assets/Cola_1/title.jpg";
 
-const imagePaths = [
-  require("./assets/Cola_1/1.jpg"),
-  require("./assets/Cola_1/2.jpg"),
-  require("./assets/Cola_1/3.jpg"),
-  require("./assets/Cola_1/4.jpg"),
-  require("./assets/Cola_1/5.jpg"),
-  require("./assets/Cola_1/6.jpg"),
-  require("./assets/Cola_1/7.jpg"),
-  require("./assets/Cola_1/8.jpg"),
-  require("./assets/Cola_1/9.jpg"),
-  require("./assets/Cola_1/10.jpg"),
-  require("./assets/Cola_1/11.jpg"),
-  require("./assets/Cola_1/12.jpg"),
-  require("./assets/Cola_1/13.jpg"),
-  require("./assets/Cola_1/14.jpg"),
-  require("./assets/Cola_1/15.jpg"),
-  require("./assets/Cola_1/16.jpg"),
-  require("./assets/Cola_1/17.jpg"),
-  require("./assets/Cola_1/18.jpg"),
-  require("./assets/Cola_1/19.jpg"),
-  require("./assets/Cola_1/20.jpg"),
-  require("./assets/Cola_1/21.jpg"),
-  require("./assets/Cola_1/22.jpg"),
-  require("./assets/Cola_1/23.jpg"),
-  require("./assets/Cola_1/24.jpg"),
-  require("./assets/Cola_1/25.jpg"),
-  require("./assets/Cola_1/26.jpg"),
-  require("./assets/Cola_1/27.jpg"),
-  require("./assets/Cola_1/28.jpg"),
-  require("./assets/Cola_1/29.jpg"),
-  require("./assets/Cola_1/30.jpg"),
-  // Add paths for the remaining images
-];
+// const imagePaths = [
+//   require("./assets/Cola_1/1.jpg"),
+//   require("./assets/Cola_1/2.jpg"),
+//   require("./assets/Cola_1/3.jpg"),
+//   require("./assets/Cola_1/4.jpg"),
+//   require("./assets/Cola_1/5.jpg"),
+//   require("./assets/Cola_1/6.jpg"),
+//   require("./assets/Cola_1/7.jpg"),
+//   require("./assets/Cola_1/8.jpg"),
+//   require("./assets/Cola_1/9.jpg"),
+//   require("./assets/Cola_1/10.jpg"),
+//   require("./assets/Cola_1/11.jpg"),
+//   require("./assets/Cola_1/12.jpg"),
+//   require("./assets/Cola_1/13.jpg"),
+//   require("./assets/Cola_1/14.jpg"),
+//   require("./assets/Cola_1/15.jpg"),
+//   require("./assets/Cola_1/16.jpg"),
+//   require("./assets/Cola_1/17.jpg"),
+//   require("./assets/Cola_1/18.jpg"),
+//   require("./assets/Cola_1/19.jpg"),
+//   require("./assets/Cola_1/20.jpg"),
+//   require("./assets/Cola_1/21.jpg"),
+//   require("./assets/Cola_1/22.jpg"),
+//   require("./assets/Cola_1/23.jpg"),
+//   require("./assets/Cola_1/24.jpg"),
+//   require("./assets/Cola_1/25.jpg"),
+//   require("./assets/Cola_1/26.jpg"),
+//   require("./assets/Cola_1/27.jpg"),
+//   require("./assets/Cola_1/28.jpg"),
+//   require("./assets/Cola_1/29.jpg"),
+//   require("./assets/Cola_1/30.jpg"),
+//   // Add paths for the remaining images
+// ];
 
 function App() {
   return (
-    <Swiper
-      effect={"cube"}
-      zoom={true}
-      navigation={true}
-      lazy={true}
-      pagination={{
-        clickable: true,
+    // <Swiper
+    //   effect={"cube"}
+    //   zoom={true}
+    //   navigation={true}
+    //   lazy={true}
+    //   pagination={{
+    //     clickable: true,
+    //   }}
+    //   modules={[Zoom, Navigation, Pagination, EffectCube]}
+    //   className="mySwiper"
+    //   flipEffect={{}}
+    // >
+    //   <SwiperSlide>
+    //     <img src={titleImage} alt="title" />
+    //   </SwiperSlide>
+    //   {imagePaths.map((imagePath, index) => (
+    //     <SwiperSlide key={index}>
+    //       <img src={imagePath} alt={`Page ${index + 1}`} />
+    //     </SwiperSlide>
+    //   ))}
+    // </Swiper>
+    <div
+      style={{
+        position: "relative",
+        paddingTop: "max(60%,324px)",
+        width: "100%",
+        height: "0",
       }}
-      modules={[Zoom, Navigation, Pagination, EffectCube]}
-      className="mySwiper"
-      flipEffect={{}}
     >
-      <SwiperSlide>
-        <img src={titleImage} alt="title" />
-      </SwiperSlide>
-      {imagePaths.map((imagePath, index) => (
-        <SwiperSlide key={index}>
-          <img src={imagePath} alt={`Page ${index + 1}`} />
-        </SwiperSlide>
-      ))}
-    </Swiper>
+      <iframe
+        style={{
+          position: "absolute",
+          border: "none",
+          width: "100%",
+          height: "100%",
+          left: "0",
+          top: "0",
+        }}
+        title="memories"
+        src="https://online.fliphtml5.com/vwsfi/vlzn/"
+        seamless="seamless"
+        scrolling="no"
+        frameBorder="0"
+        allowtransparency="true"
+        allowFullScreen="true"
+      ></iframe>
+    </div>
   );
 }
 
